5db003ba702344f2abe5ca35606a34ed68c51b23
detectivesteve5 <60126490+detectivesteve5@users.noreply.github.com>
2020-10-09 23:10:19 -0600

    Trying to get movement

--- New Unity Project/Assets/Scripts/Movement.cs
+++ New Unity Project/Assets/Scripts/Movement.cs
@@ -5,15 +5,52 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public Sprite Up;
+    public Sprite Down;
+    public Sprite Right;
+    public Sprite Left;
+    public float speed = 1;
+    // Use this for initialization
     void Start()
     {
-        
+
+      
     }
 
-    // Update is called once per frame
+    public float panSpeed = 20f;
+
     void Update()
     {
-        gameObject.transform.Translate(0.0f, 1, 0.0f);
+        Vector3 move;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            GetComponent<SpriteRenderer>().sprite = Up;
+            move = new Vector2(0, speed * Time.deltaTime);
+
+            transform.position += move;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            GetComponent<SpriteRenderer>().sprite = Left;
+            move = new Vector2(speed * Time.deltaTime, 0);
+
+            transform.position -= move;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            GetComponent<SpriteRenderer>().sprite = Right;
+            move = new Vector2(speed * Time.deltaTime, 0);
+
+            transform.position += move;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            GetComponent<SpriteRenderer>().sprite = Down;
+            move = new Vector2(0, speed * Time.deltaTime);
+
+            transform.position -= move;
+        }
     }
 }
